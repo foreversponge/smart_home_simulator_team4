485e3752a2d771a12189faf1c69fb4ed243adc12
Sam <huangs.wk.0508@gmail.com>
2020-10-18 01:41:50 -0400

    Simulation parameters update: added choose date, connected user management window, and room selection

--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -9,88 +9,90 @@ import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import models.User;
+import models.UserModel;
+import models.UserModel;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
     private Stage currentState;
-    private ObservableList<User> userData = FXCollections.observableArrayList();
-    private ObservableList<User> tempUserData = FXCollections.observableArrayList();
+    private ObservableList<UserModel> userData = FXCollections.observableArrayList();
+    private ObservableList<UserModel> tempUserData = FXCollections.observableArrayList();
+    private UserModel loggedUser;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception  {
+    public void start(Stage primaryStage) throws Exception {
         currentState = primaryStage;
-        try{
+        try {
             setHouseLayoutWindow();
-
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-    public void CloseWindow(){
+
+    public void closeWindow() {
         currentState.close();
     }
 
-    public ObservableList<User> getPersonData() {
-        return userData;
+    public ObservableList<UserModel> getPersonData() {
+            return userData;
     }
 
-    public void setPersonData(ObservableList<User> userData) {
+    public void setPersonData(ObservableList<UserModel> userData) {
         this.userData = userData;
     }
 
-    public ObservableList<User> getTempPersonData() {
+    public ObservableList<UserModel> getTempPersonData() {
         return tempUserData;
     }
 
-    public void setTempPersonData(ObservableList<User> tempUserData) {
+    public void setTempPersonData(ObservableList<UserModel> tempUserData) {
         this.tempUserData = tempUserData;
+    }
+
+    public UserModel getLoggedUser() {
+        return this.loggedUser;
+    }
+
+    public void setLoggedUser(UserModel loggedUser) {
+        this.loggedUser = loggedUser;
     }
 
     public void setHouseLayoutWindow() throws IOException {
         currentState.setTitle("Smart Home Simulator");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/houseLayout.fxml"));
         Parent root = fxmlLoader.load();
-        HouseLayoutController houselay = fxmlLoader.getController();
-        houselay.setMainController(this);
+        HouseLayoutController houseLayout = fxmlLoader.getController();
+        houseLayout.setMainController(this);
         Scene houseLayoutScene = new Scene(root);
         currentState.setScene(houseLayoutScene);
         currentState.show();
-
-
     }
 
-
-    public void setSimulatorParameterWindow() throws IOException {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/simulatorParameter.fxml"));
+    public void setSimulationParametersWindow() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/simulationParameters.fxml"));
         Parent root = fxmlLoader.load();
-        SimulatorParameterController simParcontroller = fxmlLoader.getController();
-        simParcontroller.setMainController(this);
-        Scene simScene = new Scene(root);
-        currentState.setScene(simScene);
+        SimulationParametersController simParController = fxmlLoader.getController();
+        simParController.setMainController(this);
+        Scene simulationParametersScene = new Scene(root);
+        currentState.setScene(simulationParametersScene);
         currentState.show();
-
-
     }
-    public void setUserManagerWindow() throws IOException{
 
+    public void setUserManagerWindow() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/userManager.fxml"));
         Parent root = fxmlLoader.load();
         UserManagerController simParcontroller = fxmlLoader.getController();
-        Stage editstage= new Stage();
-        simParcontroller.setMaincontroller(this,editstage);
+        Stage editstage = new Stage();
+        simParcontroller.setMaincontroller(this, editstage);
         editstage.initOwner(currentState);
         tempUserData.clear();
-        userData.forEach((user)->{
+        userData.forEach((user) -> {
             tempUserData.add(user);
         });
         editstage.initModality(Modality.WINDOW_MODAL);
